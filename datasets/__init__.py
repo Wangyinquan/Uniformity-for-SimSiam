cfec3cbe452cabf9c6e6b04812881c5a68af03af
Wangyinquan <48272387+Wangyinquan@users.noreply.github.com>
2021-06-04 01:43:48 +0800

    Add files via upload

---
+++ datasets/__init__.py
@@ -0,0 +1,32 @@
+import torch
+import torchvision
+from .random_dataset import RandomDataset
+
+
+def get_dataset(dataset, data_dir, transform, train=True, download=False, debug_subset_size=None, linear_eval=False):
+    if dataset == 'mnist':
+        dataset = torchvision.datasets.MNIST(data_dir, train=train, transform=transform, download=download)
+    elif dataset == 'stl10':
+        if linear_eval:
+            dataset = torchvision.datasets.STL10(data_dir, split='train' if train else 'test', transform=transform, download=download)
+        else:
+            dataset = torchvision.datasets.STL10(data_dir, split='train+unlabeled' if train else 'test', transform=transform, download=download)
+    elif dataset == 'cifar10':
+        dataset = torchvision.datasets.CIFAR10(data_dir, train=train, transform=transform, download=download)
+    elif dataset == 'cifar100':
+        dataset = torchvision.datasets.CIFAR100(data_dir, train=train, transform=transform, download=download)
+    elif dataset == 'imagenet':
+        dataset = torchvision.datasets.ImageFolder(
+        data_dir+'/train' if train==True else data_dir+'/val', 
+        transform=transform)
+#         dataset = torchvision.datasets.ImageNet(data_dir, split='train' if train == True else 'val', transform=transform, download=download)
+    elif dataset == 'random':
+        dataset = RandomDataset()
+    else:
+        raise NotImplementedError
+
+    if debug_subset_size is not None:
+        dataset = torch.utils.data.Subset(dataset, range(0, debug_subset_size)) # take only one batch
+        dataset.classes = dataset.dataset.classes
+        dataset.targets = dataset.dataset.targets
+    return dataset
