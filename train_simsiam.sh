c9853caf563805c7c2aa6d9156a940cd008cb0da
Wangyinquan <48272387+Wangyinquan@users.noreply.github.com>
2021-06-04 11:00:05 +0800

    Create train_simsiam.sh

---
+++ train_simsiam.sh
@@ -0,0 +1,8 @@
+python main.py \
+--data_dir /tmp/CIFAR/ \
+--log_dir ./logs/ \
+-c configs/simsiam_cifar10_simple.yaml \
+--ckpt_dir /tmp/ckpts/ \
+--train_url xxx \
+--out_dim 128 \
+--hide_progress
