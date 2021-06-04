59a53a5c6134d4975774bdcee8f875c2eafcd332
Wangyinquan <48272387+Wangyinquan@users.noreply.github.com>
2021-06-04 10:58:41 +0800

    Delete linear_eval.sh

--- linear_eval.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-python linear_eval.py \
---data_dir /tmp/CIFAR/ \
---log_dir ./logs/ \
--c configs/simsiam_cifar_eval_sgd.yaml \
---ckpt_dir /tmp/ckpts/ \
---eval_from xxx\
---hide_progress
